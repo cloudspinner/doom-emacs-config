9d29989ccdeaec3d0c6543d9e581880a8664676a
cloudspinner <36537951+cloudspinner@users.noreply.github.com>
2023-07-11 20:04:17 +0200

    Add lisp & clojure setup to config.el

--- config.el
+++ config.el
@@ -74,3 +74,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)                                                                                                              
+(add-hook 'emacs-lisp-mode-hook #'evil-paredit-mode)                                                                                                                
+(add-hook 'clojure-mode-hook #'enable-paredit-mode)                                                                                                                 
+(add-hook 'clojure-mode-hook #'evil-paredit-mode)
+(after! cider
+  (setq cider-clojure-cli-aliases ":dev"))
